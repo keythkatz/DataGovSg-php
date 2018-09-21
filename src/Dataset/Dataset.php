566884b1c9b0b8292534147c85774db8a2f3a0ed
Jeremy Lim <git@jerl.im>
2018-09-22 01:39:40 +0800

    Reformat code using phpfmt

--- src/Dataset/Dataset.php
+++ src/Dataset/Dataset.php
@@ -1,28 +1,35 @@
 <?php
-declare(strict_types=1);
+declare (strict_types = 1);
 
 namespace KeythKatz\DataGovSg\Dataset;
 
-abstract class Dataset {
+abstract class Dataset
+{
 
-	protected static $path;
-	protected $parameters = [];
+    protected static $path;
+    protected $parameters = [];
 
-	protected function addParameter(string $name, string $data) {
-		$this->parameters[$name] = $data;
-	}
+    protected function addParameter(string $name, string $data)
+    {
+        $this->parameters[$name] = $data;
+    }
 
-	protected function populateData() {
-		$url = \KeythKatz\DataGovSg\DataGovSg::API_URL . static::$path . "?";
-		$first = true;
-		foreach ($this->parameters as $name => $data) {
-			if ($first) $first = false;
-			else $url .= "&";
-			$url .= $name . "=" . $data;
-		}
+    protected function populateData()
+    {
+        $url = \KeythKatz\DataGovSg\DataGovSg::API_URL . static::$path . "?";
+        $first = true;
+        foreach ($this->parameters as $name => $data) {
+            if ($first) {
+                $first = false;
+            } else {
+                $url .= "&";
+            }
 
-		$guzzle = new \GuzzleHttp\Client();
-		$response = $guzzle->request("GET", $url);
-		echo($response->getBody());
-	}
+            $url .= $name . "=" . $data;
+        }
+
+        $guzzle = new \GuzzleHttp\Client();
+        $response = $guzzle->request("GET", $url);
+        echo ($response->getBody());
+    }
 }
