34b5c58eeab79347dbb3be3df638afc368210643
Jeremy Lim <git@jerl.im>
2018-09-22 02:29:34 +0800

    Implement first helper

--- src/Dataset/Dataset.php
+++ src/Dataset/Dataset.php
@@ -6,14 +6,28 @@ namespace KeythKatz\DataGovSg\Dataset;
 abstract class Dataset
 {
 
+    /**
+     * /path/to/dataset on api.data.gov.sg. Necessary to override.
+     * @var string
+     */
     protected static $path;
+
     protected $parameters = [];
 
+    /**
+     * Add a parameter to be sent in the get request.
+     * @param string $name
+     * @param string $data
+     */
     protected function addParameter(string $name, string $data)
     {
         $this->parameters[$name] = $data;
     }
 
+    /**
+     * Fetch, decode, and set to itself the data from
+     * the API with the set parameters.
+     */
     protected function populateData()
     {
         $url = \KeythKatz\DataGovSg\DataGovSg::API_URL . static::$path . "?";
--- src/Dataset/Psi.php
+++ src/Dataset/Psi.php
@@ -6,21 +6,41 @@ namespace KeythKatz\DataGovSg\Dataset;
 class Psi extends Dataset
 {
 
+    private static $regions = ["national", "north", "south", "east", "west", "central"];
+
     protected static $path = "/v1/environment/psi";
 
     public function __construct(\DateTime $date = null, bool $useTime = false)
     {
-        if ($date === null) {
-            $date = new \DateTime();
-        }
-        if (!$useTime) {
-            $dateString = $date->format("Y-m-d");
-            $this->addParameter("date", $dateString);
-        } else {
-            $dateString = $date->format("r");
-            $this->addParameter("date_time", $dateString);
+        if ($date !== null) {
+            if (!$useTime) {
+                $dateString = $date->format("Y-m-d");
+                $this->addParameter("date", $dateString);
+            } else {
+                $dateString = $date->format("r");
+                $this->addParameter("date_time", $dateString);
+            }
         }
 
         $this->populateData();
+    }
+
+    private function validateRegion(string $region)
+    {
+        if (!in_array(strtolower($region), self::$regions)) {
+            throw new InvalidArgumentException("\"$region\" is not in the list of regions.");
+        }
+    }
+
+    /**
+     * Get the reading from the first item in the fetched data.
+     * @param  string $region Region of the data. Must match what is available on the API.
+     * @return int
+     */
+    public function getO3SubIndex(string $region): int
+    {
+        $this->validateRegion($region);
+
+        return $this->items[0]->readings->o3_sub_index->$region;
     }
 }
